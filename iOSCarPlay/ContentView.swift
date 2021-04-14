eee80b98b5b1815cbaf7a503cdd5e6d90dd7a5e9
mgb615 <m4dpup@gmail.com>
2021-04-14 13:36:38 -0500

    begin the framework for app selection

--- iOSCarPlay/ContentView.swift
+++ iOSCarPlay/ContentView.swift
@@ -9,13 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            HStack() {
+                Button {
+                    
+                } label: {
+                    IconView()
+                        .padding(.leading, 4)
+                }
+                
+                Button {
+                    
+                } label: {
+                    IconView()
+                        .padding(.trailing, 4)
+                }
+            }
+        }
     }
 }
 
+struct IconView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .frame(width: geometry.size.width, height: geometry.size.width, alignment: .top)
+                .clipped(antialiased: true)
+        }
+    }
+}
+
+struct IconShade: View {
+    var body: some View {
+        GeometryReader { geometry in
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .frame(width: geometry.size.width, height: geometry.size.width, alignment: .top)
+                .clipped(antialiased: true)
+                .foregroundColor(Color.gray)
+                .opacity(0.5)
+        }
+    }
+}
+
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
