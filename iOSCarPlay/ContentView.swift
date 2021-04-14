b3208336a8773b561b3b949281207a0a20a7e632
mgb615 <m4dpup@gmail.com>
2021-04-14 08:29:37 -0500

    Initial Commit

---
+++ iOSCarPlay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSCarPlay
+//
+//  Created by Maddox on 4/14/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSCarPlay/iOSCarPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCarPlayApp.swift
+//  iOSCarPlay
+//
+//  Created by Maddox on 4/14/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOSCarPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
