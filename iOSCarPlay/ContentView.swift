f103e707be3a9717feb2a5f2dc76b9ce672dcd2e
mgb615 <m4dpup@gmail.com>
2021-04-15 14:09:14 -0500

    create rudimentary version of MapView

--- iOSCarPlay/ContentView.swift
+++ iOSCarPlay/ContentView.swift
@@ -6,24 +6,76 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
         GeometryReader { geometry in
-            HStack() {
-                Button {
-                    
-                } label: {
-                    IconView()
-                        .padding(.leading, 4)
+
+            Image("Wallpaper")
+                .resizable()
+                .ignoresSafeArea()
+
+            HStack {
+                ZStack {
+                    Rectangle()
+                        .frame(width: 80, height: .infinity, alignment: .trailing)
+                        .foregroundColor(Color.black)
+                        .edgesIgnoringSafeArea(.bottom)
+                        .opacity(0.8)
+                        .blur(radius: 0)
+
+                    VStack(spacing: 10) {
+                        
+                        // 1st App
+                        Button {
+                            
+                        } label: {
+                            Image("MapsIcon")
+                                .resizable()
+                                .frame(width: 57, height: 57, alignment: .top)
+                                .cornerRadius(10)
+                                .padding(.top,10)
+                                .padding(.leading,4)
+                            
+                        }
+                        
+                        // 2nd App
+                        Button {
+                            
+                        } label: {
+                            IconView()
+                                .padding(.leading,4)
+                                
+                        }
+                        
+                        // 3rd App
+                        Button {
+                            
+                        } label: {
+                            IconView()
+                                .padding(.leading,4)
+                        }
+                        
+                        // 4th App
+                        Button {
+                            
+                        } label: {
+                            IconView()
+                                .padding(.leading,4)
+                        }
+                        
+                        // 5th App
+                        Button {
+                            
+                        } label: {
+                            IconView()
+                                .padding(.leading,4)
+                        }
+                    }
                 }
-                
-                Button {
-                    
-                } label: {
-                    IconView()
-                        .padding(.trailing, 4)
-                }
+                MapView()
+                    .edgesIgnoringSafeArea(.all)
             }
         }
     }
@@ -31,30 +83,29 @@ struct ContentView: View {
 
 struct IconView: View {
     var body: some View {
-        GeometryReader { geometry in
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .frame(width: geometry.size.width, height: geometry.size.width, alignment: .top)
-                .clipped(antialiased: true)
-        }
+        RoundedRectangle(cornerRadius: 10, style: .continuous)
+            .frame(width: 57, height: 57, alignment: .top)
+            .ignoresSafeArea()
     }
 }
 
-struct IconShade: View {
-    var body: some View {
-        GeometryReader { geometry in
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .frame(width: geometry.size.width, height: geometry.size.width, alignment: .top)
-                .clipped(antialiased: true)
-                .foregroundColor(Color.gray)
-                .opacity(0.5)
-        }
+// Maps
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        let mapView = MKMapView()
+        return mapView
+    }
+
+    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapView>) {
     }
 }
 
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
 #endif
